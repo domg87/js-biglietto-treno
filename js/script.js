40e5391db1fe22aa979fdb22f7b671d32a9120ca
dom <domepiro87@yahoo.it>
2020-08-04 15:54:15 +0200

    base

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// 1. QUANTI KM DEVE PERCORRERE IL PASSEGGERO?
+var km = parseInt(prompt("Quanti km devi percorrere?"));
+// 2. QUANTI ANNI HA IL PASSEGGERO?
+var eta = parseInt(prompt("Quanti annni ha il passeggero?"));
+// 3. IL PREZZO DEL BIGLIETTO è DI 0.21€ AL KM
+var prezzo = km * 0.21;
+console.log(prezzo)
+// 4. SCONTO DEL 20% SE MINORI DI 18 ANNI
+// 5. SCONTO DEL 40% SE OVER 65
